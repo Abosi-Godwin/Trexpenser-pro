9a9c255e93dc0d26649964c56d1e06fc5d51177a
Abosi-Godwin <bbnl6060@github.com>
2025-09-07 21:28:25 +0100

    updated

--- src/Dashboard/features/budgets/EditBudgetForm.jsx
+++ src/Dashboard/features/budgets/EditBudgetForm.jsx
@@ -1,136 +1,3 @@
-/*import { useState } from "react";
-import Input from "../../ui/Input";
-import Button from "../../ui/Button";
-import DateInput from "../../ui/DateInput";
-
-import Modal from "../../ui/Modal";
-
-function EditBudgetForm({ data, onFormClose, onFormSubmit }) {
-    const budgetCategory = data.category;
-    const [amount, setAmount] = useState(data.amount);
-    const [startDate, setStartDate] = useState(data.startDate);
-    const [endDate, setEndDate] = useState(data.endDate);
-
-    const inputValue = event => event.target.value;
-
-    function handleAmountChange(e) {
-        setAmount(+inputValue(e));
-    }
-    function handleStartDateChange(e) {
-        setStartDate(inputValue(e));
-    }
-    function handleEndDateChange(e) {
-        setEndDate(inputValue(e));
-    }
-
-    function handleFormSave(e) {
-        e.preventDefault();
-
-        if (budgetCategory === "" || +amount === 0) return;
-
-        const budgetData = {
-            category: budgetCategory,
-            amount: +amount,
-            startDate,
-            endDate,
-            Id: data.Id
-        };
-        onFormSubmit(budgetData);
-        onFormClose();
-    }
-    return (
-        <Modal>
-            <div
-                className="p-2 bg-color-8 rounded
-    text-color-3 w-full md:w-1/2 md:p-4"
-            >
-                <div className="py-2">
-                    <h1 className="text-xl font-bold text-color-3">
-                        Edit your {budgetCategory} buget tracking
-                    </h1>
-                </div>
-                <div>
-                    <form>
-                        <div className="flex justify-between items-center gap-4">
-                            <div>
-                                <label htmlFor="BudgetAmount">
-                                    New amount:
-                                </label>{" "}
-                                <br />
-                                <Input
-                                    inputType="number"
-                                    initialValue={amount}
-                                    max=""
-                                    onHandleInputChange={handleAmountChange}
-                                    placeholder="Enter a new amount..."
-                                />
-                            </div>
-                        </div>
-                        <div className="py-4">
-                            <h1 className="capitalize text-xl">
-                                Edit the time frame
-                            </h1>
-                            <div
-                                className="flex justify-between items-center gap-4
-                        py-2"
-                            >
-                                <div className="">
-                                    <label htmlFor="">Start:</label> <br />
-                                    <DateInput
-                                        date={startDate}
-                                        setDate={setStartDate}
-                                        maxDate=""
-                                        minDate={startDate}
-                                        style="w-full outline-none rounded
-                    text-color-8 bg-color-2 p-2"
-                                        onHandleDateChange={
-                                            handleStartDateChange
-                                        }
-                                    />
-                                </div>
-
-                                <div className="">
-                                    <label htmlFor="">End:</label> <br />
-                                    <DateInput
-                                        date={endDate}
-                                        setDate={setEndDate}
-                                        maxDate=""
-                                        minDate={startDate}
-                                        style="w-full outline-none rounded
-                    text-color-8 bg-color-2 p-2"
-                                        onHandleDateChange={handleEndDateChange}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="flex justify-between py-2">
-                            <Button
-                                text="Cancel"
-                                style="w-32 bg-color-2 uppercase p-2 rounded text-color-8
-        hover:bg-color-6 hover:text-color-2 font-bold text-xl"
-                                onButtonClick={onFormClose}
-                            />
-
-                            <Button
-                                text="Edit"
-                                style="w-32 bg-color-6 uppercase p-2 rounded
-                            text-color-2
-        hover:bg-red-200 hover:text-color-2 font-bold text-xl"
-                                onButtonClick={handleFormSave}
-                            />
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </Modal>
-    );
-}
-
-export default EditBudgetForm;
-
-*/
-
 import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
@@ -143,66 +10,30 @@ import SelectInput from "../Form/SelectInput";
 import TextArea from "../Form/TextArea";
 
 import { useTransactions } from "../../Hooks/useTransactions";
-import { useUser } from "../../Hooks/useUser";
-import { useBudgets } from "../../Hooks/useBudgets";
-import { useAddBudget } from "../../Hooks/useAddBudget";
-
 function EditBudgetForm({ data, type, onCloseForm }) {
-    console.log(data);
+    const { amount, category, start_date, end_date, notes } = data;
+
     const { expenseCategories: budgetCategories } = useTransactions();
-
-    const { user } = useUser();
-    const userId = user.id;
-
-    const { budgets } = useBudgets(userId);
-    const {
-        addBudget,
-        addedBudget,
-        isAddingBudget,
-        isAddingBudgetError,
-        isAddingBudgetSucces
-    } = useAddBudget();
 
     const {
         register,
         watch,
         handleSubmit,
         formState: { errors }
-    } = useForm({defaultValues:{}});
+    } = useForm({
+        defaultValues: {
+            amount,
+            category,
+            description: notes,
+            start: start_date,
+            end: end_date
+        }
+    });
 
     const startDate = watch("start");
 
-    const [currentDate, setCurrentDate] = useState("");
-
     function handleFormCancel() {
         onCloseForm();
-    }
-
-    const doNothing = () => {
-        console.log("Existing category");
-    };
-
-    function submitBudget(
-        category,
-        amount,
-        start,
-        end,
-        userId,
-        isActive,
-        description
-    ) {
-        const budgetData = {
-            category,
-            amount,
-            start_date: start,
-            end_date: end,
-            status: isActive,
-            user_id: userId,
-            notes: description || "No description added."
-        };
-        addBudget(budgetData, {
-            onSuccess: onCloseForm()
-        });
     }
 
     function handleFormSave(infos) {
@@ -213,30 +44,14 @@ function EditBudgetForm({ data, type, onCloseForm }) {
             return;
         }
 
-        const existingCategory = (selectedCategory, initialBudgets) =>
-            initialBudgets.some(budget => budget.category === selectedCategory);
-
-        const hasBudget = existingCategory(category, budgets);
-
-        const isActive = end >= currentDate ? "active" : "expired";
-
-        hasBudget
-            ? toast.error(`${category} is already being tracked.`)
-            : submitBudget(
-                  category,
-                  +amount,
-                  start,
-                  end,
-                  userId,
-                  isActive,
-                  description
-              );
+        console.log(
+            category,
+            +amount,
+            start,
+            end,
+            description
+        );
     }
-
-    useEffect(() => {
-        const today = new Date().toISOString().split("T")[0];
-        setCurrentDate(today);
-    }, []);
 
     return (
         <Modal>
@@ -281,7 +96,7 @@ function EditBudgetForm({ data, type, onCloseForm }) {
                             <DateInput
                                 label="start"
                                 maxDate=""
-                                minDate={currentDate}
+                                minDate={start_date}
                                 className="w-full outline-none border-none rounded-md  p-2 bg-light-sectionBackground"
                                 register={register}
                             />
